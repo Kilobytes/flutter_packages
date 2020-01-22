88da244bb1c5382e9d68afb523d9c10567ea66b7
darkcompet <darkcompet@gmail.com>
2020-01-22 15:37:14 +0700

    update load images

--- flutter_core/lib/core.dart
+++ flutter_core/lib/core.dart
@@ -13,6 +13,8 @@ export 'src/log/benchmark.dart';
 export 'src/util/maths.dart';
 export 'src/util/objects.dart';
 export 'src/util/strings.dart';
+export 'src/util/images.dart';
+export 'src/util/utils.dart';
 
 const bool DEBUG = kDebugMode;
 const bool RELEASE = kReleaseMode;
